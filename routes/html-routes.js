e1cca1779325e4e6bcf7a5feab31a5a55d795eb1
An Phan <antphan98@gmail.com>
2020-01-31 19:19:39 -0600

    added pet sign up page and linked to every page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -19,7 +19,11 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/request-sitter.html"))
     });
 
-//    app.get("*", function(req, res) {
-//        res.sendFile(path.join(__dirname, "../public/index.html"));
-//      });
+    app.get("/petsignup", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/pet-signup.html"))
+    });
+
+   app.get("*", function(req, res) {
+       res.sendFile(path.join(__dirname, "../public/index.html"));
+     });
 }
